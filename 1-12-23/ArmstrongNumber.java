3916673ecb10572187f654fe3c169baeb7efce2c
Akshada2727 <149786386+Akshada2727@users.noreply.github.com>
2023-12-01 21:20:10 +0530

    Add files via upload

---
+++ 1-12-23/ArmstrongNumber.java
@@ -0,0 +1,37 @@
+
+ package company;
+   import java.util.Scanner;
+
+
+  public class ArmstrongNumber {
+
+		
+		
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				int n,c,rem,arm=0;
+				
+				System.out.println("Enter any number ");
+				Scanner sc = new Scanner(System.in);
+				n = sc.nextInt();
+				
+				c=n;
+				
+				while(n>0)
+				{
+					rem=n%10;
+					arm=(rem*rem*rem)+arm;
+					n=n/10;
+				}
+					if(c==arm)
+						System.out.println("Armstrong number ");
+					else
+						System.out.println("Not Armstrong number ");
+						
+				
+
+			}
+
+		}
+
+	
