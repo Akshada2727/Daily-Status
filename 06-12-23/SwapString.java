34a9427d4eae8ec0fadc10dd66b12e42aa9e409d
Akshada2727 <149786386+Akshada2727@users.noreply.github.com>
2023-12-06 22:43:17 +0530

    Add files via upload

---
+++ 06-12-23/SwapString.java
@@ -0,0 +1,21 @@
+package String;
+
+public class SwapString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        String str1 = "Good ", str2 = "bad ";    
+		         System.out.println("Strings before swapping: " + str1 + " " + str2);    
+		      
+		        str1 = str1 + str2;    
+		         
+		        str2 = str1.substring(0, (str1.length() - str2.length()));    
+		        
+		        str1 = str1.substring(str2.length());    
+		            
+		        System.out.println("Strings after swapping: " + str1 + " " + str2);    
+		    }    
+		}    
+
+	
