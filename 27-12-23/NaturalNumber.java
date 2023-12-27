cbc43179d7989a8562a5fcdc98a7821550a156ed
Akshada2727 <149786386+Akshada2727@users.noreply.github.com>
2023-12-27 18:44:01 +0530

    Add files via upload

---
+++ 27-12-23/NaturalNumber.java
@@ -0,0 +1,13 @@
+package company;
+import java.util.stream.IntStream;
+
+public class NaturalNumber {
+
+			    public static void main(String[] args) 
+		    {
+		        int sum = IntStream.range(1, 11).sum();
+		         
+		        System.out.println(sum);
+		    }
+		}
+
