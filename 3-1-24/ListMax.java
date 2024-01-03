a24a271acf17480e36639c57a23dceca4a2ac85e
Akshada2727 <149786386+Akshada2727@users.noreply.github.com>
2024-01-03 21:52:25 +0530

    Add files via upload

---
+++ 3-1-24/ListMax.java
@@ -0,0 +1,12 @@
+package practice;
+import java.util.*;
+
+public class ListMax {
+
+		   public static void main(String[] args) {
+		      List list = Arrays.asList("one Two three Four five six one three Four".split(" "));
+		      System.out.println(list);
+		      System.out.println("max: " + Collections.max(list));
+		      System.out.println("min: " + Collections.min(list));
+		   }
+		}
